a5240664441b76015b3c3289cf9e44c12d3bedc7
Pablo Caro Revuelta <pcaro@yaco.es>
2011-05-16 18:23:26 +0200

    Support for relative-paths
    Because sometimes you want to be able to move a buildout directory
    around and have scripts still work without having to rebuild them
    Read: http://pypi.python.org/pypi/zc.buildout/1.5.2#id146

--- djangoprojectrecipe/recipe.py
+++ djangoprojectrecipe/recipe.py
@@ -71,7 +71,11 @@ class Recipe(object):
             ]
         if self.extra_paths:
             options['extra-paths'] = '\n'.join(self.extra_paths)
-        
+
+        self._relative_paths = buildout['buildout'].get('relative-paths', False)
+        if self._relative_paths == 'true':
+            self._relative_paths = options['buildout-directory']
+
 
     def install(self):
         requirements, ws = self.egg.working_set(['djangoprojectrecipe'])
@@ -102,7 +106,8 @@ class Recipe(object):
                   'djangoprojectrecipe.manage', 'main')],
                 ws, self.options['executable'], self.options['bin-directory'],
                 extra_paths = extra_paths,
-                arguments= "'%s'" % (site_config['settings_module']))
+                arguments= "'%s'" % (site_config['settings_module'],),
+                relative_paths = self._relative_paths,)
         )
         return scripts
 
@@ -127,7 +132,9 @@ class Recipe(object):
                         extra_paths=extra_paths,
                         arguments= "'%s', logfile='%s'" % (
                             site_config['settings_module'],
-                            self.options.get('logfile'))))
+                            self.options.get('logfile')),
+                        relative_paths = self._relative_paths,
+                        ))
         zc.buildout.easy_install.script_template = _script_template
         return scripts
 

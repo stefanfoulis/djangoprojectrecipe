4ac04e942245539fa384c804d0eb597390bfd099
stefanfoulis <stefan.foulis@gmail.com>
2011-05-18 08:41:24 +0200

    minor cleanup in relative-paths code

--- djangoprojectrecipe/recipe.py
+++ djangoprojectrecipe/recipe.py
@@ -65,9 +65,10 @@ class Recipe(object):
         if self.extra_paths:
             options['extra-paths'] = '\n'.join(self.extra_paths)
 
-        self._relative_paths = buildout['buildout'].get('relative-paths', False)
-        if self._relative_paths == 'true':
-            self._relative_paths = options['buildout-directory']
+        if buildout['buildout'].get('relative-paths', '') == 'true':
+            self.relative_paths = options['buildout-directory']
+        else:
+            self.relative_paths = False
 
     def install(self):
         requirements, ws = self.egg.working_set(['djangoprojectrecipe'])
@@ -99,7 +100,7 @@ class Recipe(object):
                 ws, self.options['executable'], self.options['bin-directory'],
                 extra_paths=extra_paths,
                 arguments="'%s'" % (site_config['settings_module']),
-                relative_paths=self._relative_paths
+                relative_paths=self.relative_paths
             )
         )
         return scripts
@@ -125,7 +126,7 @@ class Recipe(object):
                         arguments="'%s', logfile='%s'" % (
                             site_config['settings_module'],
                             self.options.get('logfile')),
-                        relative_paths = self._relative_paths,
+                        relative_paths = self.relative_paths,
                         ))
         zc.buildout.easy_install.script_template = _script_template
         return scripts
